0bc356af2c503b6061969f5ac579af1c5de7d984
cbkewagamang <cbkewagamang@gmail.com>
2025-10-20 16:40:04 +0200

    integrated API data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-function displayTemperature(response) {
+function displayWeatherData(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.innerHTML = `<img
+      src="${response.data.condition.icon_url}"
+      class="current-temperature-icon"
+    />`;
+
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 }
 
 function search(event) {
@@ -14,7 +27,7 @@ function search(event) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayWeatherData);
 }
 
 function formatDate(date) {
